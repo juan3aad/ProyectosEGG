52b16abd3fbcec2c9fa528234e362b922c915851
Juan Alexander Agreda <juan3aad@gmail.com>
2023-04-13 22:09:08 -0500

    Realizando la correción del codigo

---
+++ If/src/pkgif/If.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgif;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ajuand
+ */
+public class If {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner leer = new  Scanner(System.in);
+        // TODO code application logic here
+        //Pedri los numeros
+        System.out.println("Escriba el primer numero:");
+        int num1=leer.nextInt();
+        System.out.println("Escriba el siguiente numero");
+        int num2=leer.nextInt();
+        
+        
+    }
+    
+}
