9a9a20de0e0ac4156666d175291c8fd8560ee1f3
Juan Alexander Agreda <juan3aad@gmail.com>
2023-04-24 20:22:45 -0500

    Realizar la busqueda de matriz si esta contenida

--- Extras/src/extras/Ejer21_BuscarMatriz.java
+++ Extras/src/extras/Ejer21_BuscarMatriz.java
@@ -20,7 +20,7 @@ public class Ejer21_BuscarMatriz {
         // TODO code application logic here
 
         int[][] m = new int[10][10];
-        int[][] p = new int[10][10];
+        int[][] p = new int[3][3];
 
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
